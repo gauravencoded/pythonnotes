b4ccf24e0d51de0061e58a05b820866c3def103c
Gaurav <singhgaurav4242@gmail.com>
2019-05-05 22:34:35 +0530

    input function replaced with raw_input check for difference

--- rsa.py
+++ rsa.py
@@ -105,7 +105,7 @@ if __name__ == '__main__':
     print ("Generating your public/private keypairs now . . .")
     public, private = generate_keypair(p, q)
     print ("Your public key is ", public ," and your private key is ", private)
-    message = input("Enter a message to encrypt with your private key: ")
+    message = raw_input("Enter a message to encrypt with your private key: ")
     encrypted_msg = encrypt(private, message)
     print ("Your encrypted message is: ")
     print (''.join(map(lambda x: str(x), encrypted_msg)))
